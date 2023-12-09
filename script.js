e9f33385728864dc87fbf48c1e76323941a323b9
ducphucdo <153350960+ducphucdo@users.noreply.github.com>
2023-12-09 21:13:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+
+	
+	//About page
+	
+	$(".about-text").on('click', function () {
+		$("body").addClass("about-on");
+	});
+	$(".about-close").on('click', function () {
+		$("body").removeClass("about-on");
+	});
+
+	
+	//Contact page
+	
+	$(".contact-text").on('click', function () {
+		$("body").addClass("contact-on");
+	});
+	$(".contact-close").on('click', function () {
+		$("body").removeClass("contact-on");
+	});
+
+	
+	//Equipments portfolio page
+	
+	$(".equipments").on('click', function () {
+		$("body").addClass("equipments-on");
+	});
+	$(".equipments-close").on('click', function () {
+		$("body").removeClass("equipments-on");
+	});
+
+	
+	//Process portfolio page
+	
+	$(".process").on('click', function () {
+		$("body").addClass("process-on");
+	});
+	$(".process-close").on('click', function () {
+		$("body").removeClass("process-on");
+	});
+
+	
+	//Extras portfolio page
+	
+	$(".extras").on('click', function () {
+		$("body").addClass("extras-on");
+	});
+	$(".extras-close").on('click', function () {
+		$("body").removeClass("extras-on");
+	});
+
+	
+})(jQuery); 
